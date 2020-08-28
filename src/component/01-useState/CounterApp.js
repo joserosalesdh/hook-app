e44fc2800e30fac7cf64794340ce54fd93ee78ea
joserosalesdh <jm.rosales92@gmail.com>
2020-08-28 11:42:41 -0300

    custom hook

---
+++ src/component/01-useState/CounterApp.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './counter.css';
+
+const CounterApp = () => {
+
+    const [state, setState] = useState({
+        counter1: 10,
+        counter2: 20,
+        counter3: 30,
+        counter4: 40
+    });
+
+    const { counter1, counter2 } = state;
+
+    return (
+        <>
+            <h1>Counter1 {counter1} </h1>
+            <h1>Counter2 {counter2} </h1>
+            <hr />
+
+            <button
+                className="btn btn-primary"
+                onClick={() => {
+                    setState({
+                        ...state, //Como estoy buscando solo aumentar en uno al contador 1, para que me mantenga los mismos valores del estado orginal tengo que usar un spread operator asi los otros contadores quedan como estan y no desaparecen en el neuvo estado setState
+                        counter1: counter1 + 1
+                    });
+                }}
+            >
+                +1
+            </button>
+        </
+        >
+    )
+}
+
+export default CounterApp
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import HookApp from './HookApp';
+// import CounterApp from './component/01-useState/CounterApp';
+import CounterWithCustomHook from './component/01-useState/CounterWithCustomHook';
+
+ReactDOM.render(
+
+  <CounterWithCustomHook />,
+
+  document.getElementById('root')
+);
+
+
