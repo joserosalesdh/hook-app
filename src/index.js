b55c09ba49208a413151302e0fc1bda21c0a371c
joserosalesdh <jm.rosales92@gmail.com>
2020-08-28 12:34:35 -0300

    comit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SimpleForm } from './component/02-useEffect/SimpleForm';
 // import HookApp from './HookApp';
 // import CounterApp from './component/01-useState/CounterApp';
-import CounterWithCustomHook from './component/01-useState/CounterWithCustomHook';
+// import CounterWithCustomHook from './component/01-useState/CounterWithCustomHook';
 
 ReactDOM.render(
 
-  <CounterWithCustomHook />,
+  <SimpleForm />,
 
   document.getElementById('root')
 );
