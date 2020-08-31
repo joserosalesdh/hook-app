fed0247723178c24136837c73aad547c4a204dae
joserosalesdh <jm.rosales92@gmail.com>
2020-08-31 11:07:46 -0300

    comit

--- src/component/02-useEffect/SimpleForm.js
+++ src/component/02-useEffect/SimpleForm.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './effects.css'
+import Message from './Message';
 
 export const SimpleForm = () => {
 
@@ -11,15 +12,15 @@ export const SimpleForm = () => {
     const { name, email } = formState;
 
     useEffect(() => {
-        console.log('hey!');
+        // console.log('hey!');
     }, []);
 
     useEffect(() => {
-        console.log('formState cambió');
+        // console.log('formState cambió');
     }, [formState]);
 
     useEffect(() => {
-        console.log('email cambió');
+        // console.log('email cambió');
     }, [email]);
 
     const handleInputChange = ({ target }) => {
@@ -60,7 +61,7 @@ export const SimpleForm = () => {
                 />
             </div>
 
-
+            {(name === '123') && <Message />}
         </>
     )
 }
