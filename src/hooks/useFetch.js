02a0eae2678b688d25532b13edc454f48279eb83
joserosalesdh <jm.rosales92@gmail.com>
2020-09-04 10:31:25 -0300

    pruebas en useFetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -30,6 +30,13 @@ export const useFetch = (url) => {
                 }
 
             })
+            .catch(() => {
+                setState({
+                    data: null,
+                    loading: false,
+                    error: 'No se pudo cargar la info'
+                })
+            })
 
     }, [url])
 
