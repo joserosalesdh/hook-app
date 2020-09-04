24e65cabcd7c27fc53eaba79d21cdc2819da477d
joserosalesdh <jm.rosales92@gmail.com>
2020-09-04 11:12:52 -0300

    Pruebas en MultipleCustonHooks

---
+++ src/test/hooks/components/03-examples/MultipleCustonHooks.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import '@testing-library/jest-dom';
+import { MultipleCustonHooks } from '../../../../component/03-examples/MultipleCustonHooks';
+import { useFetch } from '../../../../hooks/useFetch';
+import { useCounter } from '../../../../hooks/useCounter';
+jest.mock('../../../../hooks/useFetch'); // Esto hace que cuando yo vaya a utilizar en este archivo, entonces en lugar de usar useFetch va a utilizar una implementación una implementacion que voy a inventar del useFetch
+// el jest.mock hace la simulación del useFetch
+jest.mock('../../../../hooks/useCounter');
+
+describe('Pruebas en <MultipleCustonHooks />', () => {
+
+    useCounter.mockReturnValue({
+        counter: 10,
+        increment: () => { } // Pongo una funcion cualquiera para que no me tire error
+    });
+
+    test('debe de mostrarse correctamente', () => {
+
+        useFetch.mockReturnValue({
+            data: null,
+            loading: true,
+            error: null
+        })
+
+        const wrapper = shallow(<MultipleCustonHooks />); // No recive ningun argumento
+
+        expect(wrapper).toMatchSnapshot();
+
+    });
+
+    test('debe de mostrar la información', () => {
+
+        useFetch.mockReturnValue({
+            data: [{
+                author: 'Jose',
+                quote: 'Hola Mundo'
+            }],
+            loading: false,
+            error: null
+        });
+
+        const wrapper = shallow(<MultipleCustonHooks />);
+
+        expect(wrapper.find('.alert').exists()).toBe(false); //No deberia de existir es lo que quiero decir aca
+        expect(wrapper.find('.mb-0').text().trim()).toBe('Hola Mundo');
+        expect(wrapper.find('footer').text().trim()).toBe('Jose');
+
+    })
+
+
+
+})
